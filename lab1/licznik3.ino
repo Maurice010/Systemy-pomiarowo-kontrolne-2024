d8c980599ddc582c4ec7d32c6a68a2ec0f79bc1c
Jakub Kozak <85180405+Maurice010@users.noreply.github.com>
2024-03-19 22:16:51 +0100

    Add files via upload

--- lab1/licznik3.ino
+++ lab1/licznik3.ino
@@ -1,78 +1,84 @@
-// #############################
-// Wyswietlacz 8-segmentowy:
-//
-//     A
-//    F B
-//     G
-//    E C
-//     D .dot(dp)
-//
-// binary - dpGFE DCBA
-// swiecenie, wybor cyfry = LOW
-// #############################
+// 1. Serial.println("test") (w przykladzie ReadASCIIString sluzyc do debugowania (patrzymy co sie dzieje na porcie klikajac lupee po prawej stronie)
+// 2. Wpisujemy liczbe 4-cyfrowa - procesor ma ja ustawic na wyswietlaczu 7-segmentowym
+// Najpierw wyswietlamy cyfry 0 - 9 na wyswietlaczu:
+// Wybieramy port 8-bitowy i podpinamy (zeby zaświecić to dajemy LOW na SEGMENT i CYFRA)
+// Potem robimy odliczanie jak w kuchence mikrofalowej 1. 2 starsze cyfry to liczba min, 2 mlodsze to sekundy, tam gdzie podzial kropka
+// Potem odliczanie dopiero po nacisnieciu przycisku
 
+// A
+//F B
+// G
+//E C
+// D dot(dp)
+//
+//binary - dpGFE DCBA
+//swiecenie, wybor cyfry = LOW
+const int button = 16;
 void setup() {
   DDRB = 255;
   DDRA = 255;
-  DDRC = 0;
   PORTA = 0;
   PORTB = 0;
   Serial.begin(9600);
   pinMode(button, INPUT_PULLUP);
 }
 
-const int button = 16;
-
 const byte nums[] = {0b11000000, 0b11111001, 0b10100100, 0b10110000, 0b10011001, 0b10010010, 0b10000010, 0b11111000, 0b10000000, 0b10011000};
 const byte numsDot[] = {0b01000000, 0b01111001, 0b00100100, 0b00110000, 0b00011001, 0b00010010, 0b00000010, 0b01111000, 0b00000000, 0b00011000};
 
-unsigned long t1;
-unsigned long t2;
-bool buttonPressed = false;
+unsigned long t1 = 0;
+unsigned long t2 = 0;
+
+byte buttonPressed = false;
 
 void loop() {
     byte button = PINC & 1;
-    int num = Serial.parseInt();
-
-    Serial.println(num);
+    int num = 0;
+    num = Serial.parseInt();
   
     int cyfra[] = {num / 1000, (num / 100) % 10, (num / 10) % 10, num % 10};
 
-    Serial.println(cyfra[0]);
-    Serial.println(cyfra[1]);
-    Serial.println(cyfra[2]);
-    Serial.println(cyfra[3]);
-
-    if(button == LOW) {
-      t1 = millis();
-      buttonPressed = true;
-    }
-
-    while(Serial.available() <= 0) {
-      for(int i = 0; i < 4; i++) {
-        if(i == 1) {
-          PORTB = numsDot[cyfra[i]];
+    while(Serial.available() > 0) {
+        button = PINC & 1;
+        if(button == LOW && buttonPressed == false) {
+          t1 = millis();
+          buttonPressed = true;
         }
-        else {
-          PORTB = nums[cyfra[i]];
+        for(int i = 0; i < 4; i++) {
+          if(i == 1) {
+            PORTB = numsDot[cyfra[i]];
+          }
+          else {
+            PORTB = nums[cyfra[i]];
+          }
+          PORTA = ~(1 << i);
+          delay(1); // potrzebny, diody nie sa w stanie tak szybko sie swiecic gdyby nie bylo
         }
-        PORTA = ~(1 << i);
-        delay(1); // diody nie sa w stanie tak szybko sie swiecic gdyby nie bylo
+        t2 = millis();
+        
+        if((t2 - t1) >= 1000 && num > 0 && buttonPressed == true) {
+          int sec = num % 100;
+          int mins = num / 100;
+          if(sec <= 0 && mins > 0) {
+            mins--;
+            num = mins * 100 + 59;
+          }
+          else if(sec > 0) {
+            sec--;
+            num = mins * 100 + sec;
+          }
+            
+          cyfra[0] = num / 1000;
+          cyfra[1] = (num / 100) % 10;
+          cyfra[2] = (num / 10) % 10;
+          cyfra[3] = num % 10;
+          t1 = t2;
+          
+        }
       }
-      t2 = millis();
-
-      if((t2 - t1) >= 1000 && num > 0 && buttonPressed == true) {
-        num--;
-        cyfra[0] = num / 1000;
-        cyfra[1] = (num / 100) % 10;
-        cyfra[2] = (num / 10) % 10;
-        cyfra[3] = num % 10;
-        t1 = millis();
-      }
-    }
 
     PORTA = 255;
-    buttonPressed = false;
+    buttonPressed == false;
 }
 
 
